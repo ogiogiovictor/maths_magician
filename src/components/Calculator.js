da3443e2caa0438643c098dca6a5f6000fcc04a7
ogiogiovictor@gmail.com <ogiogiovictor@gmail.com>
2022-09-08 12:43:46 -0600

    functionalComponent

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,66 +1,61 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import Screen from './screen/screen';
 import Button from './button/button';
 import calculate from './logic/calculate';
 
 import './calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
 
-    this.state = {
-      total: null,
-      next: null,
-    };
-  }
+  const showContent = (content) => {
+    setState(() => ({ next: content }));
+  };
 
-    showContent = (content) => {
-      this.setState(() => ({ next: content }));
-    }
+  const act = (opr) => {
+    // console.log(operation)
+    setState((state) => (calculate(state, opr)));
+  };
 
-    act = (opr) => {
-      // console.log(operation)
-      this.setState((state) => (calculate(state, opr)));
-    }
+  const button = [
+    { text: 'AC', type: 'normal' },
+    { text: '+/-', type: 'normal' },
+    { text: '%', type: 'normal' },
+    { text: '÷', type: 'orange' },
+    { text: '7', type: 'normal' },
+    { text: '8', type: 'normal' },
+    { text: '9', type: 'normal' },
+    { text: 'x', type: 'orange' },
+    { text: '4', type: 'normal' },
+    { text: '5', type: 'normal' },
+    { text: '6', type: 'normal' },
+    { text: '-', type: 'orange' },
+    { text: '1', type: 'normal' },
+    { text: '2', type: 'normal' },
+    { text: '3', type: 'normal' },
+    { text: '+', type: 'orange' },
+    { text: '0', type: 'normal double' },
+    { text: '.', type: 'normal' },
+    { text: '=', type: 'orange' },
+  ];
 
-    render() {
-      const { next, total, operation } = this.state;
+  return (
+    <>
+      <div className="letsCalculate">Lets do some calculations</div>
+      <div className="cover-area">
+        <Screen
+          info={state.next || state.operation || state.total}
+          onChange={(e) => showContent(e.target.value)}
+        />
 
-      const button = [
-        { text: 'AC', type: 'normal' },
-        { text: '+/-', type: 'normal' },
-        { text: '%', type: 'normal' },
-        { text: '÷', type: 'orange' },
-        { text: '7', type: 'normal' },
-        { text: '8', type: 'normal' },
-        { text: '9', type: 'normal' },
-        { text: 'x', type: 'orange' },
-        { text: '4', type: 'normal' },
-        { text: '5', type: 'normal' },
-        { text: '6', type: 'normal' },
-        { text: '-', type: 'orange' },
-        { text: '1', type: 'normal' },
-        { text: '2', type: 'normal' },
-        { text: '3', type: 'normal' },
-        { text: '+', type: 'orange' },
-        { text: '0', type: 'normal double' },
-        { text: '.', type: 'normal' },
-        { text: '=', type: 'orange' },
-      ];
-
-      return (
-        <div className="cover-area">
-          <Screen info={next || operation || total} onChange={(e) => this.showContent(e.target.value)} />
-
-          <div className="calculator-grid">
-            {button.map((btn) => (
-              <Button key={btn.text} text={btn.text} type={btn.type} act={this.act} />
-            ))}
-          </div>
+        <div className="calculator-grid">
+          {button.map((btn) => (
+            <Button key={btn.text} text={btn.text} type={btn.type} act={act} />
+          ))}
         </div>
-      );
-    }
-}
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
